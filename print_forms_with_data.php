b3ca06f1145b633a9056fb344c50a51076a3c166
Susan Lowry <sklowry@umn.edu>
2016-04-07 15:53:30 -0500

    Did cleanup in print_forms_with_data - deleted commented out code, use $Proj->forms & $user_rights['forms'] instead of SQLqueries,fixed Bugs

--- print_forms_with_data.php
+++ print_forms_with_data.php
@@ -3,6 +3,8 @@
  * PLUGIN NAME: print_forms_with_data.php
  * DESCRIPTION: This displays the forms for a project in a format that could be copied and pasted into Word
  *              and includes the data from the record for which the ID is passed in
+ *              Required parameter: pid for project id
+ *              Optional parameter: lws=y for less white space (e.g. ?pid=1&lws=y)
  * VERSION:     1.0
  * AUTHOR:      Sue Lowry - University of Minnesota
  */
@@ -22,31 +24,8 @@ if (PROJECT_ID != 24) {
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
 // Your HTML page content goes here
-?>
-        <style type="text/css">
-                table {border-collapse:collapse;}
-                table.ReportTableWithBorder {
-                  border-right:none;
-                  border-left:none;
-                  border-right:1pt solid black;
-                  border-bottom:1pt solid black;
-                  font-size:17px;
-                  font-size:13px;
-                  font-family:helvetica,arial,sans-serif;
-                }
-                .ReportTableWithBorder th,
-                .ReportTableWithBorder td {
-                  border-top: none;
-                  border-left: none;
-                  border-top: 1pt solid black;
-                  border-left: 1pt solid black;
-                  padding: 4px 5px;
-                  font-weight:normal;
-                }
-                td.DataA {background-color:##DDDDDD;}
-                td.DataB {background-color:##FFFFFF;}
-        </style>
-<?php
+if ($_REQUEST['lws'] == 'y') { $lws = true; $not_lws = false; } else { $lws = false; $not_lws = true; }
+
 $record = $_REQUEST['record'];
 $this_pid = $_REQUEST['pid'];
 
@@ -99,104 +78,41 @@ if ($user_dag > "") {
 	}
 }
 
-// Get a list of the forms for the project that the user has access to
-if (!SUPER_USER) {
-    $sql = sprintf( "
-        SELECT m.field_order, m.form_menu_description, m.form_name
-          FROM redcap_metadata m, redcap_user_rights u
-         WHERE m.project_id =%d
-           AND m.form_menu_description > ''
-           AND u.project_id = m.project_id
-           AND u.username = '%s' 
-           AND (u.data_entry like concat('%s[', m.form_name, ',1]%s') OR 
-                u.data_entry like concat('%s[', m.form_name, ',2]%s') OR 
-                u.data_entry like concat('%s[', m.form_name, ',3]%s') )
-         ORDER BY m.field_order",
-                 $this_pid, $userid, '%', '%', '%', '%', '%', '%' );
-} else {
-    $sql = sprintf( "
-        SELECT m.field_order, m.form_menu_description, m.form_name
-          FROM redcap_metadata m
-         WHERE m.project_id =%d
-           AND m.form_menu_description > ''
-         ORDER BY m.field_order",
-                 $this_pid);
-}
-
-// execute the sql statement
-$form_result = $conn->query( $sql );
-if ( ! $form_result )  // sql failed
-{
-        die( "Could not execute SQL: <pre>$sql</pre> <br />" .  mysqli_error($conn) );
-}
-
 // Build the ddl field with the list of forms
 $ddl = 'Select a data collection instrument to view <select id="record_select1" onchange="';
 $ddl .= "var disp='none'; ";
-$ddl .= "var fld = document.getElementById('record_select1'); ";
-//$ddl .= "alert('fld.value: ' + fld.value); ";
-$ddl .= "if (fld.value == '') { disp=''; } ";
-//$ddl .= "if (document.getElementById('record_select1').value > '') { disp=''; } ";
-//$ddl .= "alert('disp: ' + disp); ";
-while ($form_record = $form_result->fetch_assoc( ))
+$ddl .= "if (document.getElementById('record_select1').value == '') {disp='';} ";
+foreach ($Proj->forms as $form_name=>$attr)
 {
-//	$ddl .= 'if (document.) { } else { }; ';
-	$ddl .= "document.getElementById('".$form_record['form_name']."_form').style.display=disp;";
+    if ($user_rights['forms'][$form_name] > 0)
+    {
+	$ddl .= "document.getElementById('".$form_name."_form').style.display=disp;";
+    }
 }
 $ddl .= "document.getElementById(document.getElementById('record_select1').value + '_form').style.display='';".'">';
-$form_result = $conn->query( $sql );
 $ddl .= '<option value="">- select an instrument -</option>';
-while ($form_record = $form_result->fetch_assoc( ))
+foreach ($Proj->forms as $form_name=>$attr)
 {
-	$ddl .= '<option value="'.$form_record['form_name'].'">'.$form_record['form_menu_description'].'</option>' ;
+    if ($user_rights['forms'][$form_name] > 0)
+    {
+	$ddl .= '<option value="'.$form_name.'">'.$attr['menu'].'</option>' ;
+    }
 }
 $ddl .= '</select>';
 
-//print '<table border="0" cellpadding="2" cellspacing="0" class="ReportTableWithBorder">';
 // PRINT PAGE button
 print  "<div style='text-align:right;width:700px;max-width:700px;'>". $ddl ."
              <button class='jqbuttonmed' onclick='window.print();'><img src='".APP_PATH_IMAGES."printer.png' class='imgfix'> Print page</button>
          </div>";
 
-// Get a list of the forms in the project
-if (!SUPER_USER) {
-    $sql = sprintf( "
-        SELECT m.field_order, m.form_menu_description, m.form_name
-          FROM redcap_metadata m, redcap_user_rights u
-         WHERE m.project_id =%d
-           AND m.form_menu_description > ''
-           AND u.project_id = m.project_id
-           AND u.username = '%s'
-           AND (u.data_entry like concat('%s[', m.form_name, ',1]%s') OR
-                u.data_entry like concat('%s[', m.form_name, ',2]%s') OR
-                u.data_entry like concat('%s[', m.form_name, ',3]%s') )
-         ORDER BY m.field_order",
-                 $this_pid, $userid, '%', '%', '%', '%', '%', '%' );
-} else {
-    $sql = sprintf( "
-        SELECT m.field_order, m.form_menu_description, m.form_name
-          FROM redcap_metadata m
-         WHERE m.project_id =%d
-           AND m.form_menu_description > ''
-         ORDER BY m.field_order",
-                 $this_pid);
-}
-
-// execute the sql statement
-$form_result = $conn->query( $sql );
-if ( ! $form_result )  // sql failed
-{
-        die( "Could not execute SQL: <pre>$sql</pre> <br />" .  mysqli_error($conn) );
-}
-
-//print '<table border="0" cellpadding="2" cellspacing="0" class="ReportTableWithBorder">';
-//print '<div style="max-width:700px;width:700px;"><table border="0" cellpadding="2" cellspacing="0" id="form_table" class="form_border">';
 // Loop through all the forms
-while ($form_record = $form_result->fetch_assoc( ))
+foreach ($Proj->forms as $form_name=>$attr)
 {
+    if ($user_rights['forms'][$form_name] > 0)
+    {
 ?>
-    <div id="<?php echo $form_record['form_name'] ?>_form" style="max-width:700px;">
-	<h3 style="color:#800000;max-width:700px;border-bottom:2pt solid #800000;font-size:175%;"><?php echo $form_record['form_menu_description'] ?></h3>
+    <div id="<?php echo $form_name ?>_form" style="max-width:700px;">
+	<h3 style="color:#800000;max-width:700px;border-bottom:2pt solid #800000;font-size:175%;"><?php echo $attr['menu'] ?></h3>
 
 <?php
 
@@ -217,7 +133,7 @@ while ($form_record = $form_result->fetch_assoc( ))
                    AND   m.form_name = '%s'
                    /* AND   m.element_type <> 'descriptive' */
                    ORDER BY m.field_order",
-                      $record, $this_pid, $form_record['form_name'] );
+                      $record, $this_pid, $form_name );
 
 
         // execute the sql statement
@@ -243,7 +159,7 @@ while ($form_record = $form_result->fetch_assoc( ))
                 $field_name = $fields_record['field_name'];
  		$field_value = $fields_record['field_value'];
 		// Don't print form_complete fields
-		if ( $field_name == $form_record['form_name'] . '_complete' ) { continue; }
+		if ( $field_name == $form_name . '_complete' ) { continue; }
 
                 if ( $fields_record['element_enum'] > "" and  $fields_record['element_type'] != 'calc') {
                         $element_enums = explode('|',str_replace('\n','|',$fields_record['element_enum'])) ;
@@ -265,10 +181,13 @@ while ($form_record = $form_result->fetch_assoc( ))
 
 
                 $print_type = "";
-                if ($fields_record['element_enum'] > "") {
+                if ($fields_record['element_enum'] > "" and  $fields_record['element_type'] != 'calc') {
                         if ( $fields_record['element_type'] == 'sql' ) {
 //                                $print_type .= '<table border="0" cellpadding="2" cellspacing="0" class="ReportTableWithBorder"><tr><td>' . $fields_record['element_enum'] . '</td></tr></table>';
                         } else {
+				if ($fields_record['custom_alignment'] == "LH" and $not_lws) {
+                                	$print_type .= '<br />';
+                                }
 				$all_vals = array();
 				$all_vals[1] = $field_value;
 				if ($fields_record['element_type'] == 'checkbox' and $record > "" and $fld_nbr < count($variableHash)) {
@@ -282,7 +201,7 @@ while ($form_record = $form_result->fetch_assoc( ))
 					$val = substr($this_element_enum, 0, $pos);
 					$label = substr($this_element_enum, $pos + 1);
 					if ($record > "" and array_search($val, $all_vals) > 0 ) { $x_or_space = "X"; } else { $x_or_space = "&nbsp;"; }
-						if (substr($fields_record['custom_alignment'], -1) == "H") {
+						if (substr($fields_record['custom_alignment'], -1) == "H" or $lws) {
                                                 	if ($fields_record['element_type'] == 'checkbox' ) {
 								$print_type .= '&nbsp; &nbsp; [ '.$x_or_space.' ] '.$label;
                                                		} else {
@@ -297,9 +216,6 @@ while ($form_record = $form_result->fetch_assoc( ))
                                         		$print_type .= '<br />';
 						}
                                 }
-				if ($fields_record['custom_alignment'] == "LH") {
-                                	$print_type .= '<br />';
-                                }
                         }
                 }
 		elseif ($fields_record['element_type'] == 'descriptive') {
@@ -307,16 +223,20 @@ while ($form_record = $form_result->fetch_assoc( ))
 		}
 		elseif ($fields_record['element_type'] == 'textarea') {
                     if ($fields_record['element_note'] > "") {
-                	if (substr($fields_record['element_note'], 1, 1) == "(") {
-                        	$print_type .= " ".$fields_record['element_note']."<br/>";
+                	if (substr($fields_record['element_note'], 0, 1) == "(") {
+                        	$print_type .= " ".$fields_record['element_note'];
                 	} else {
-                        	$print_type .= " (".$fields_record['element_note'].")<br/>";
+                        	$print_type .= " (".$fields_record['element_note'].")";
                 	}
 		    }
 		    if ($record > "" and $field_value > "") {
-		        $print_type .= '<br /><u> '.$field_value.' </u>';
+		        $print_type .= '<br /><u> '.str_replace(chr(13),'<br/>',$field_value).' </u>';
 		    } else {
-		        $print_type .= '<br /><br /><hr><br /><hr><br /><hr>';
+		        if ($not_lws) { 
+                            $print_type .= '<br /><br /><hr><br /><hr><br /><hr>'; 
+                        } else {
+			    $print_type .= ' ______________________________________________________________________';
+                        }
 		    }
 		}
 		elseif ($fields_record['element_validation_type'] == 'date_mdy') {
@@ -365,29 +285,35 @@ while ($form_record = $form_result->fetch_assoc( ))
 		}
                 if ($fields_record['element_note'] > "") {
 		    if ($fields_record['element_type'] != 'textarea') {
-                	if (substr($fields_record['element_note'], 1, 1) == "(") {
-                        	$print_type .= " ".$fields_record['element_note']."<br/>";
+                	if (substr($fields_record['element_note'], 0, 1) == "(") {
+                        	$print_type .= " ".$fields_record['element_note'];
                 	} else {
-                        	$print_type .= " (".$fields_record['element_note'].")<br/>";
+                        	$print_type .= " (".$fields_record['element_note'].")";
                 	}
+                        if ($not_lws) { $print_type .= '<br />';}
                     }
                 }
 
                 // Print the preceding header above the field, if there is one
                 if ( $this_element_preceding_header > "" ) {
-                        print '<br />'.str_replace('<hr>','<hr style="color:black;">',$fields_record['element_preceding_header']) . "<br />";
+			$hr = ( strpos(' '.$fields_record['element_preceding_header'], '<hr') );
+			if ($not_lws or $hr == false) { print '<br />'; }
+                        print str_replace('<hr>','<hr style="color:black;">',$fields_record['element_preceding_header']);
+                        if ($not_lws) { print '<br />'; }
                 }
                 // Print the information about the field
                	print $print_label;
-                if ($fields_record['element_enum'] > "" && substr($fields_record['custom_alignment'], -1) != "H") {
+                if ($fields_record['element_enum'] > "" && substr($fields_record['custom_alignment'], -1) != "H" && $not_lws && $fields_record['element_type'] != 'calc') {
                		print "<br />" . $print_type;
 		} else {
                		print " &nbsp; " . $print_type . "<br />";
 		}
-                print "<br />";
+                if ($not_lws) { print "<br />"; }
         }
-	print "<br /><br /><br />";
+	print "<br />";
+	if ($not_lws) {print "<br /><br />"; }
     print "</div>";
+    }
 }
 
 
